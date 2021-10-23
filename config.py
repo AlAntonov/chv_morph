937a489028e39ea39c3fbcd98a1bb429f34845cf
Alexander Antonov <alexantonov@gmail.com>
2021-10-23 20:51:24 +0300

    add lemma and wordform

--- config.py
+++ config.py
@@ -18,6 +18,8 @@ class Config:
     self.search_filename = 'chv.100K.monocorpus.txt'
     self.index_filename = 'index.npy'
     self.verb_index_filename = 'verb_index.npy'
+    self.cash_filename = 'cash_index.npy'
+    self.vocab_filename = 'vocab_index.npy'
     self.has_second_word = has_second_word
   
     def chv_read_verbs_list(verb_filename):
@@ -46,6 +48,7 @@ class Config:
       lico_list = ['1', '2', '3']
       negative_list = ['p', 'n']
       verb_form_list = ['nb', 'po', 'bb', 'ps', 'pd', 'im', 'in', 'pn', 'pb', 'pm', 'de', 'du', 'dv']
+      form_in_config = False
       with open(config_filename, encoding="utf-8") as config_file:
         for line in config_file:
           splitted_line = line.replace('\n','').split(':')
@@ -54,17 +57,22 @@ class Config:
               return form_list
           if splitted_line[0] == 'Число':
             chislo_list = splitted_line[1].split(',')
+            form_in_config = True
           if splitted_line[0] == 'Лицо':
             lico_list = splitted_line[1].split(',')
+            form_in_config = True
           if splitted_line[0] == 'Отрицание':
             negative_list = splitted_line[1].split(',')
+            form_in_config = True
           if splitted_line[0] == 'Форма':
             verb_form_list = splitted_line[1].split(',')
-      for chislo in chislo_list:
-        for lico in lico_list:
-          for negative in negative_list:
-            for verb_form in verb_form_list:
-              form_list.append(chislo + '_' + lico + '_' + negative + '_' + verb_form)
+            form_in_config = True
+      if form_in_config:
+        for chislo in chislo_list:
+          for lico in lico_list:
+            for negative in negative_list:
+              for verb_form in verb_form_list:
+                form_list.append(chislo.strip() + '_' + lico.strip() + '_' + negative.strip() + '_' + verb_form.strip())
       return form_list
 
     def chv_get_form_from_noun_config(config_filename):
@@ -72,6 +80,7 @@ class Config:
       chislo_list = ['e', 'm']
       lico_list = ['0','1', '2', '3']
       padezh_list = ['im', 'ro', 'da', 'me', 'is', 'tv', 'pc']
+      form_in_config = False
       with open(config_filename, encoding="utf-8") as config_file:
         for line in config_file:
           splitted_line = line.replace('\n','').split(':')
@@ -80,26 +89,50 @@ class Config:
               return form_list
           if splitted_line[0] == 'Число':
             chislo_list = splitted_line[1].split(',')
+            form_in_config = True
           if splitted_line[0] == 'Лицо':
             lico_list = splitted_line[1].split(',')
+            form_in_config = True
           if splitted_line[0] == 'Падеж':
             padezh_list = splitted_line[1].split(',')
-      for chislo in chislo_list:
-        for lico in lico_list:
-          for padezh in padezh_list:
-            form_list.append(chislo + '_' + lico + '_' + padezh)
+            form_in_config = True
+      if form_in_config:
+        for chislo in chislo_list:
+          for lico in lico_list:
+            for padezh in padezh_list:
+              form_list.append(chislo.strip() + '_' + lico.strip() + '_' + padezh.strip())
       return form_list
+    
+    def chv_get_wordform_from_config(config_filename):
+      wordform_list = []
+      is_lemma = False
+      with open(config_filename, encoding="utf-8") as config_file:
+        for line in config_file:
+          splitted_line = line.replace('\n','').split(':')
+          if splitted_line[0] == 'Словоформа':
+            wordform_list = splitted_line[1].split(',')
+          if splitted_line[0] == 'Лемма':
+            wordform_list = splitted_line[1].split(',')
+            is_lemma = True
+      return wordform_list, is_lemma
+    
 
     self.form_verb_list = chv_get_form_from_config(config_filename)
     self.form_noun_list = chv_get_form_from_noun_config(config_filename)
+    self.wordform_list, self.is_lemma = chv_get_wordform_from_config(config_filename)
     self.conj_verb_table = chv_read_conj_table(conj_verb_filename, True)
     self.conj_noun_table = chv_read_conj_table(conj_noun_filename, True)
+    self.conj_verb_normal_table = chv_read_conj_table(conj_verb_filename, False)
+    self.conj_noun_normal_table = chv_read_conj_table(conj_noun_filename, False)
     self.verbs_list = chv_read_verbs_list(verb_filename)
     self.trn_verbs_list = chv_read_verbs_list(trn_verb_filename)
     self.nouns_list = chv_read_verbs_list(noun_filename)
     self.trn_nouns_list = chv_read_verbs_list(trn_noun_filename)
     self.form2_verb_list = []
     self.form2_noun_list = []
+    self.wordform2_list = []
+    self.is_lemma2 = False
     if has_second_word >= 1:
       self.form2_verb_list = chv_get_form_from_config(config2_filename)
       self.form2_noun_list = chv_get_form_from_noun_config(config2_filename)
+      self.wordform2_list, self.is_lemma2 = chv_get_wordform_from_config(config2_filename)
